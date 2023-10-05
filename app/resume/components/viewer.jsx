78c550c953c46b6c3b1e70fa73637799c0b4bf4e
Easton Miller <j.easton.miller@gmail.com>
2023-10-05 15:37:37 -0500

    fix resume page

--- app/resume/components/viewer.jsx
+++ app/resume/components/viewer.jsx
@@ -1,43 +1,14 @@
 'use client'
 
-// import * as PDFJS from 'pdfjs-dist/build/pdf';
 
-// PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.min.js`;
+  
 
-const showPDFInViewer = async (url) => {
-
-  const WebViewer = (await (import('@pdftron/pdfjs-express'))).default;
-  
-  WebViewer(
-    {
-      path: '/webviewer/lib',
-      initialDoc: url
-    }, pageRenderRef.current).then((instance) => {
-      const { docViewer, Annotations } = instance;
-      var FitMode = instance.FitMode;
-      
-      instance.setHeaderItems((header) => {
-      header.update([]);
-    });
-  
-    var iframeWindow = instance.iframeWindow;
-    const headerItems = iframeWindow.document.querySelector('.HeaderToolsContainer');
-    headerItems.style.display = 'none';
-
-    docViewer.on('documentLoaded', () => {
-    instance.setFitMode(FitMode.FitWidth);
-    
-    });
-  
-  });
-  
-}
 
 export default function Viewer() {
 
   return (
     <div className='guide'>
-      {showPDFInViewer(".../public/assets/test.pdf")}
+      
     </div>
 
   )
